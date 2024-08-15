f4faca2ecf12a634bbada99d70982db7ebc54513
Tracy Kemboi <tkemboi72@gmail.com>
2024-08-15 21:40:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 server.use(router);
 
 server.listen(port, () => {
